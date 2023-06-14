59e65ed9d17b964ad032a560afe294e77769d956
Captn fresh <captainfresh.aa1@gmail.com>
2023-06-14 02:32:57 +0100

    Initial

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xEE046b54c9d6ea138f658B202c4C308d0c15c450";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
